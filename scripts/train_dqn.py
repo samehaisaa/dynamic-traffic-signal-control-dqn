098b3a4c2fe7141917fd72e645d5f8e33c566dc9
samehaisaa <a.semeh555@gmail.com>
2024-11-29 22:35:39 +0000

    Update train_dqn.py

--- scripts/train_dqn.py
+++ scripts/train_dqn.py
@@ -3,13 +3,10 @@ from envs.traffic_env import TrafficEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 def main():
-    # Initialize the environment and wrap it in DummyVecEnv
     env = DummyVecEnv([lambda: TrafficEnv()])
     
-    # Create the DQN model
     model = create_model(env)
 
-    # Train the model
     print("Training the DQN model...")
     train_model(model, timesteps=20000)
 
